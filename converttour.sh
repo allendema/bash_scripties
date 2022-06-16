8ac2ff0379d2d702360eef61bfd2b4e882798341
Allen <64094914+allendema@users.noreply.github.com>
2022-06-16 21:29:00 +0200

    [mod] (converttour.sh) Copy metadata + rename original files

--- converttour.sh
+++ converttour.sh
@@ -6,8 +6,8 @@ input_format=jpg
 # Output Format
 output_format=jpeg
 
-# Convert files with ffmpeg
-for i in *.$input_format; do ffmpeg -i "$i" "${i%.*}.$output_format"; done
+# Convert files with ffmpeg - Also copy metadata
+for i in *.$input_format; do ffmpeg -i "$i" -map_metadata 0 -c copy "${i%.*}.$output_format"; done
 
-# Delete files in old (original) format
-for i in *.$input_format; do rm "$i"; done
+# Rename old files
+for i in *.$input_format; do mv "$i" original_"$i"; done
